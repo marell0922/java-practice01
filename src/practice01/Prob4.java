190ca82c23ccccea4fad0a903ba5e62d6e1c0a0b
lock033 <lock0665@gmail.com>
2018-12-27 13:57:43 +0900

    ver2

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -10,12 +10,16 @@ public class Prob4 {
 		String str=scanner.nextLine();
 		scanner.close();
 		
-		for(int i=0;i<str.length();i++) {
+		/*for(int i=0;i<str.length();i++) {
 			for(int j=0; j<i+1;j++) {
 				System.out.print(str.charAt(j));
 			}
 			System.out.println();
-		}
+		}*/
+		
+		for(int i=1;i<str.length()+1;i++)
+			System.out.println(str.substring(0, i));
+			
 	}
 
 }
